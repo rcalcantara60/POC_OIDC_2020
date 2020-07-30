59a4bfa403792e56d40ff870679769685aa6eee7
Rodrigo Costa <rodrial_7@hotmail.com>
2020-07-30 14:28:33 -0300

    Add2

---
+++ src/Api/Controllers/IdentityController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]    
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize("GrupoUsuario")]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
